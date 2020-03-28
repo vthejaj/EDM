5b70202c36ff97637b27c982930a42403779ff56
vthejaj <59957350+vthejaj@users.noreply.github.com>
2020-03-28 14:46:12 +0530

    Add project files.

---
+++ EDM/ListProducts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDM
+{
+    class ListProducts
+    {
+        static void Main(string[] args)
+        {
+            msdbEntities ctx = new msdbEntities();
+            //ctx.Database.Log = Console.WriteLine;
+
+            // Eager load - bring all from products and categories 
+            foreach(product p in ctx.products.Include("category"))
+            {
+                Console.WriteLine( $"{p.ProdName} - {p.category.CatDesc}");
+            }
+        }
+    }
+}
